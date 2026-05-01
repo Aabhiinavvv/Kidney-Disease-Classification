0ba4900b2d86e694d81775a5573569ec71c585f8
Abhinav <abhinavyadav3012@gmail.com>
2026-05-01 22:39:44 +0530

    Added today's updates: <short description>

---
+++ main.py
@@ -0,0 +1,89 @@
+from pathlib import Path
+from typing import Any, Union
+import os
+import yaml
+import json
+import joblib
+import base64
+
+from box import ConfigBox, BoxValueError
+from ensure import ensure_annotations
+from cnnClassifier import logger
+
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"YAML file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise BoxValueError("YAML file is empty")
+    except Exception as e:
+        raise e
+
+
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"Created directory at: {path}")
+
+
+@ensure_annotations
+def save_json(path: Path, data: dict):
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
+    logger.info(f"JSON file saved at: {path}")
+
+
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    with open(path, "r") as f:
+        content = json.load(f)
+        logger.info(f"JSON file loaded from: {path}")
+        return ConfigBox(content)
+
+
+@ensure_annotations
+def save_bin(data: Any, path: Path):
+    joblib.dump(value=data, filename=path)
+    logger.info(f"Binary file saved at: {path}")
+
+
+@ensure_annotations
+def load_bins(path: Path) -> Any:
+    data = joblib.load(path)
+    logger.info(f"Binary loaded from: {path}")
+    return data
+
+
+@ensure_annotations
+def get_size(path: Path) -> str:
+    size_in_kb = os.path.getsize(path) / 1024
+    return f"{size_in_kb:.2f} KB"
+
+
+def decoderImage(imgstring, filename):
+    imgdata = base64.b64decode(imgstring)
+    with open(filename, "wb") as f:
+        f.write(imgdata)
+
+
+def encodeImageIntoBase64(croppedImagePath):
+    with open(croppedImagePath, "rb") as f:
+        return base64.b64encode(f.read())
+    
+
+
+    
+     
+         
+
+
+
+
+
+
--- template.py
+++ template.py
@@ -6,26 +6,32 @@ import logging
 
 logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: %(message)s:')
 
-project_name = 'cnnClassifier'
+project_name = "cnnClassifier"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
+
+    # Main package
     f"src/{project_name}/__init__.py",
+
+    # Submodules
     f"src/{project_name}/components/__init__.py",
-    f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/config/__init__.py",
-    f"src/{project_name}/config/configuration.py",
-    f"src/{project_name}/pipeline/__init__.py",
-    f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/constants/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+
+    # Config files
     "config/config.yaml",
-    "dvc.yaml",
     "params.yaml",
+    "dvc.yaml",
+
+    # Other root files
+    "main.py",
     "requirements.txt",
     "setup.py",
-    "research/trials.ipynb",
-    "templates/index.html",
-    ]
+]
 
 for filepath in list_of_files:
     filepath = Path(filepath)
