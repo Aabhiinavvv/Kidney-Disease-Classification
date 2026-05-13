593eca4cf175dc36abf38ff49e15ad5389d42e7d
Abhinav <abhinavyadavv3012@gmail.com>
2026-05-13 17:54:49 +0530

    model training fixing bug

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -27,6 +27,7 @@ class PrepareBaseModelConfig:
 @dataclass(frozen=True)
 class TrainingConfig:
     root_dir: Path
+    trained_model_path: Path
     updated_base_model_path: Path
     training_data: Path
     params_epochs: int
